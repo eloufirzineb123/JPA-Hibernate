36870733f8b67717ccc3ea8634bb0041969971f8
Filip Iricanin <irican_f@etna-alternance.net>
2022-07-22 16:20:04 +0200

    fix todo exo entity

--- jpa-hibernate-exercises/hello-jpa-entity/src/main/java/fr/m2i/model/Movie.java
+++ jpa-hibernate-exercises/hello-jpa-entity/src/main/java/fr/m2i/model/Movie.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 /**
  * TODO: Vous devez implémenter les mapping JPA pour l'entité {@link Movie}
  * - spécifier un id
+ * - spécifier le nom de la table: "movie"
  * - configurer id en tant colonne qui s'auto incrémente
  * - spécifier explicitement chaque nom de colonne ("id", "name", "director", "duration")
  * - spécifier une contrainte "not null" pour les champs {@link Movie#name} and {@link Movie#director}
